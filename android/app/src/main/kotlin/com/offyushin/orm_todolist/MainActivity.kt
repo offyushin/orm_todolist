a58ce7ef686e568370314bc14c602ef2b13f6675
Kim.Yushin <offyushin@naver.com>
2023-12-26 17:44:02 +0900

    TodoList 프로젝트 생성

---
+++ android/app/src/main/kotlin/com/offyushin/orm_todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.offyushin.orm_todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo 리스트'),
+      ),
+    );
+  }
+}
